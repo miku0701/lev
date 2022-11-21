3a2e86ed41cedb0aae9e88ee3a13900972a03052
miku0701 <miku.0817m@icloud.com>
2022-11-21 21:02:48 +0900

    8-3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\post;
+use App\Models\Post;
+
 class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return $post->get();
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(2)]);
+
     }
-
+    public function show(Post $post)
+    {
+    return view('posts/show')->with(['post'=> $post]);
+    }
 }
-
+?>
 
